aaf57deb057c1bf94907cf38a99122717995f4c0
Adomkay <sh_adom@outlook.com>
2018-07-12 14:03:17 -0400

    Done.

--- seed.py
+++ seed.py
@@ -7,3 +7,17 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # add and commit the actors and roles below
+
+tom_hanks = Actor(name = "Tom Hanks")
+tom_hanks.roles = [Role(character = "Forrest Gump"), Role(character = "Jim Lovell"), Role(character = "Woody"), Role(character = "Robert Langdon")]
+
+gwyneth_paltrow = Actor(name = "Gwyneth Paltrow")
+gwyneth_paltrow.roles = [Role(character = "Pepper Potts"), Role(character = "Margot Tenenbaum")]
+
+sam_jackson = Actor(name = "Samuel Jackson")
+sam_jackson.roles = [Role(character = "Jules Winnfield"), Role(character = "Mace Windu")]
+
+session.add(tom_hanks)
+session.add(gwyneth_paltrow)
+session.add(sam_jackson)
+session.commit()
